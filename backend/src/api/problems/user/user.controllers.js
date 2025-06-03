37213b2224d233127bcf219a24f286b3b805ecd1
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-06-04 02:54:51 +0530

    feat: implement runCode controller to execute and return code execution results

--- backend/src/api/problems/user/user.controllers.js
+++ backend/src/api/problems/user/user.controllers.js
@@ -47,7 +47,22 @@ export const getOneProblem = asyncHandler(async (req, res) => {
 });
 
 // @controller POST /:problemSlug/run-code
-export const runCode = asyncHandler(async (req, res) => {});
+export const runCode = asyncHandler(async (req, res) => {
+  // find problem by slug
+  const existingProblem = await Problem.findOne({ slug: req.params.problemSlug });
+  if (!existingProblem) throw new APIError(404, "Run Code Error", "Problem not found");
+
+  // get test cases execution result and execution status from the request
+  const { testCasesExecutionResults, executionStatus } = req.problemExecutionResults;
+
+  // success status to user
+  return res.status(200).json(
+    new APIResponse(200, "Code Executed Successfully", {
+      testCasesExecutionResults,
+      executionStatus,
+    }),
+  );
+});
 
 // @controller POST /:problemSlug/submit-code
 export const submitCode = asyncHandler(async (req, res) => {});
