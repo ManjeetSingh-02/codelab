65515187d9c14eb51ee1686b0ec7abe252853b97
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-06-04 04:02:41 +0530

    feat: implement getAllUserSubmissions and getAllProblemSubmissions controllers to fetch user submissions

--- backend/src/api/submissions/submission.controllers.js
+++ backend/src/api/submissions/submission.controllers.js
@@ -1,8 +1,38 @@
 // import local modules
 import { asyncHandler } from "../../utils/async-handler.js";
+import { APIError } from "../error.api.js";
+import { Problem } from "../problems/problem.models.js";
+import { APIResponse } from "../response.api.js";
+import { Submission } from "./submission.models.js";
 
 // @route GET /
-export const getAllUserSubmissions = asyncHandler(async (req, res) => {});
+export const getAllUserSubmissions = asyncHandler(async (req, res) => {
+  // get all submissions made by the user
+  const allSubmissions = await Submission.find({ userId: req.user.id })
+    .select("-userId -testCases -__v -updatedAt")
+    .populate("problemId", "title slug difficulty tags");
+
+  // success status to user
+  return res
+    .status(200)
+    .json(new APIResponse(200, "All user submissions fetched successfully", allSubmissions));
+});
 
 // @route GET /:problemSlug
-export const getAllProblemSubmissions = asyncHandler(async (req, res) => {});
+export const getAllProblemSubmissions = asyncHandler(async (req, res) => {
+  // get problem by slug
+  const existingProblem = await Problem.findOne({ slug: req.params.problemSlug });
+  if (!existingProblem)
+    throw new APIError(404, "Get all Problem Submissions Error", "Problem not found");
+
+  // get all submissions made by the user for a specific problem
+  const allSubmissions = await Submission.find({
+    problemId: existingProblem._id,
+    userId: req.user.id,
+  }).select("language status createdAt");
+
+  // success status to user
+  return res
+    .status(200)
+    .json(new APIResponse(200, "All problem submissions fetched successfully", allSubmissions));
+});
